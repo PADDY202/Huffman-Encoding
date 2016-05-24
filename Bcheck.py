62f27040e17033529ac0b5bc819eb1a0ba2db265
PADDY202 <padraig.mitchell@ucdconnect.ie>
2016-05-24 19:36:07 +0100

    Redundancy removed

--- Bcheck.py
+++ Bcheck.py
@@ -1,4 +1,6 @@
 import huff
-huff.Huff(2, one=eval('1/21'), two=eval('2/21'), three=eval('3/21'), four=eval('4/21'), five=eval('5/21'), six=eval('6/21'))
-huff.Huff(3, one=eval('1/21'), two=eval('2/21'), three=eval('3/21'), four=eval('4/21'), five=eval('5/21'), six=eval('6/21'))
-huff.Huff(4, one=eval('1/21'), two=eval('2/21'), three=eval('3/21'), four=eval('4/21'), five=eval('5/21'), six=eval('6/21'))
+input={'1':eval('1/21'),'2':eval('2/21'),'3':eval('3/21'),'4':eval('4/21'),'5':eval('5/21'),'6':eval('6/21')}
+symbols = list(input.keys())
+huff.Huff(2, **input)
+huff.Huff(3, **input)
+huff.Huff(4, **input)
